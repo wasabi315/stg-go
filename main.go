eaa0e83e1c30fa206eeeeedf08dfa249cc409e32
wasabi315 <wasabi.nvim.st@gmail.com>
2020-12-28 01:28:11 +0900

    Print AST

--- main.go
+++ main.go
@@ -1,9 +1,103 @@
 package main
 
 import (
-	"fmt"
+	"github.com/wasabi315/stg-go/stg"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	ast := []*stg.Bind{
+		&stg.Bind{
+			Var: stg.Var("a"),
+			LF: &stg.LF{
+				Body: &stg.LetExpr{
+					Binds: []*stg.Bind{
+						&stg.Bind{
+							Var: stg.Var("add"),
+							LF: &stg.LF{
+								Args: []stg.Var{
+									stg.Var("x"),
+									stg.Var("y"),
+								},
+								Body: &stg.PrimAppExpr{
+									Prim: stg.Prim("+#"),
+									Atoms: []stg.Atom{
+										stg.Var("x"),
+										stg.Var("y"),
+									},
+								},
+							},
+						},
+						&stg.Bind{
+							Var: stg.Var("sub"),
+							LF: &stg.LF{
+								Args: []stg.Var{
+									stg.Var("x"),
+									stg.Var("y"),
+								},
+								Body: &stg.PrimAppExpr{
+									Prim: stg.Prim("-#"),
+									Atoms: []stg.Atom{
+										stg.Var("x"),
+										stg.Var("y"),
+									},
+								},
+							},
+						},
+					},
+					Body: &stg.VarAppExpr{
+						Var: stg.Var("add"),
+						Atoms: []stg.Atom{
+							stg.Lit(10),
+							stg.Lit(20),
+						},
+					},
+				},
+			},
+		},
+		&stg.Bind{
+			Var: stg.Var("b"),
+			LF: &stg.LF{
+				Upd: true,
+				Body: &stg.CaseExpr{
+					Target: &stg.PrimAppExpr{
+						Prim: "+#",
+						Atoms: []stg.Atom{
+							stg.Lit(1),
+							stg.Lit(2),
+						},
+					},
+					Alts: []stg.Alt{
+						&stg.PAlt{
+							Lit: 0,
+							Expr: &stg.PrimAppExpr{
+								Prim: "printInt#",
+								Atoms: []stg.Atom{
+									stg.Lit(0),
+								},
+							},
+						},
+						&stg.PAlt{
+							Lit: 1,
+							Expr: &stg.PrimAppExpr{
+								Prim: "printInt#",
+								Atoms: []stg.Atom{
+									stg.Lit(1),
+								},
+							},
+						},
+						&stg.DAlt{
+							Expr: &stg.PrimAppExpr{
+								Prim: "printInt#",
+								Atoms: []stg.Atom{
+									stg.Lit(100),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	stg.PrintProgram(ast)
 }
--- stg/ast.go
+++ stg/ast.go
@@ -1,5 +1,14 @@
 package stg
 
+import (
+	"fmt"
+	"strings"
+)
+
+/*****************************************************************************/
+/* AST                                                                       */
+/*****************************************************************************/
+
 type (
 	Var  string
 	Ctor string
@@ -145,4 +154,169 @@ type LF struct {
 type Bind struct {
 	Var Var
 	LF  *LF
+}
+
+/*****************************************************************************/
+/* Print AST                                                                 */
+/*****************************************************************************/
+
+const indentWidth = 4
+
+func PrintProgram(bs []*Bind) {
+	PrintBinds(bs, 0)
+}
+
+func PrintBinds(bs []*Bind, indent int) {
+	for i, b := range bs {
+		PrintBind(b, indent)
+		fmt.Println()
+		if i != len(bs)-1 {
+			fmt.Println()
+		}
+	}
+}
+
+func PrintBind(b *Bind, indent int) {
+	PrintIndent(indent)
+	PrintVar(b.Var)
+	fmt.Print(" = ")
+	PrintLF(b.LF, indent)
+}
+
+func PrintVar(v Var) {
+	fmt.Print(v)
+}
+
+func PrintVars(vs []Var) {
+	fmt.Print("{")
+	for i, v := range vs {
+		PrintVar(v)
+		if i != len(vs)-1 {
+			fmt.Print(", ")
+		}
+	}
+	fmt.Print("}")
+}
+
+func PrintLF(lf *LF, indent int) {
+	PrintVars(lf.Free)
+
+	if lf.Upd {
+		fmt.Print(" \\u ")
+	} else {
+		fmt.Print(" \\n ")
+	}
+
+	PrintVars(lf.Args)
+
+	fmt.Println(" ->")
+
+	PrintExpr(lf.Body, indent+1)
+}
+
+func PrintExpr(e Expr, indent int) {
+	PrintIndent(indent)
+	e.VisitExpr(ExprVisitor{
+		Let: func(l *LetExpr) {
+			fmt.Println("let")
+			PrintBinds(l.Binds, indent+1)
+			PrintIndent(indent)
+			fmt.Println("in")
+			PrintExpr(l.Body, indent+1)
+		},
+		Case: func(c *CaseExpr) {
+			fmt.Print("case")
+			fmt.Print(" ")
+			PrintExpr(c.Target, 0)
+			fmt.Println(" of")
+			PrintAlts(c.Alts, indent+1)
+		},
+		VarApp: func(v *VarAppExpr) {
+			PrintVar(v.Var)
+			fmt.Print(" ")
+			PrintAtoms(v.Atoms)
+		},
+		CtorApp: func(c *CtorAppExpr) {
+			PrintCtor(c.Ctor)
+			fmt.Print(" ")
+			PrintAtoms(c.Atoms)
+		},
+		PrimApp: func(p *PrimAppExpr) {
+			PrintPrim(p.Prim)
+			fmt.Print(" ")
+			PrintAtoms(p.Atoms)
+		},
+		Lit: PrintLit,
+	})
+}
+
+func PrintCtor(c Ctor) {
+	fmt.Print(c)
+}
+
+func PrintPrim(p Prim) {
+	fmt.Print(p)
+}
+
+func PrintLit(l Lit) {
+	fmt.Print(l)
+}
+
+func PrintAtom(a Atom) {
+	a.VisitAtom(AtomVistor{
+		Var: PrintVar,
+		Lit: PrintLit,
+	})
+}
+
+func PrintAtoms(as []Atom) {
+	fmt.Print("{")
+	for i, a := range as {
+		PrintAtom(a)
+		if i != len(as)-1 {
+			fmt.Print(", ")
+		}
+	}
+	fmt.Print("}")
+}
+
+func PrintAlts(as []Alt, indent int) {
+	for i, a := range as {
+		PrintAlt(a, indent)
+		fmt.Println()
+		if i != len(as)-1 {
+			fmt.Println()
+		}
+	}
+}
+
+func PrintAlt(a Alt, indent int) {
+	PrintIndent(indent)
+	a.VisitAlt(AltVisitor{
+		AAlt: func(a *AAlt) {
+			PrintCtor(a.Ctor)
+			fmt.Print(" ")
+			PrintVars(a.Vars)
+			fmt.Println(" ->")
+			PrintExpr(a.Expr, indent+1)
+		},
+		PAlt: func(p *PAlt) {
+			PrintLit(p.Lit)
+			fmt.Println(" ->")
+			PrintExpr(p.Expr, indent+1)
+		},
+		VAlt: func(v *VAlt) {
+			PrintVar(v.Var)
+			fmt.Println(" ->")
+			PrintExpr(v.Expr, indent+1)
+		},
+		DAlt: func(d *DAlt) {
+			fmt.Println("default ->")
+			PrintExpr(d.Expr, indent+1)
+		},
+	})
+}
+
+func PrintIndent(n int) {
+	fmt.Print(strings.Repeat(" ", n*indentWidth))
 }
